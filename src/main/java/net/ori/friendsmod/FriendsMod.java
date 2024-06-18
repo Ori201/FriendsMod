27625435fb332bc775049485f3ec28854816a3de
Ori Gershov <chiburiki12@gmail.com>
2024-06-18 21:07:22 +0300

    חדש!

--- src/main/java/net/ori/friendsmod/FriendsMod.java
+++ src/main/java/net/ori/friendsmod/FriendsMod.java
@@ -1,6 +1,8 @@
 package net.ori.friendsmod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -11,6 +13,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.ori.friendsmod.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -22,6 +25,8 @@ public class FriendsMod {
 
     public FriendsMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -35,7 +40,9 @@ public class FriendsMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.DOCUMENT);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
--- src/main/java/net/ori/friendsmod/item/ModItems.java
+++ src/main/java/net/ori/friendsmod/item/ModItems.java
@@ -1,4 +1,21 @@
 package net.ori.friendsmod.item;
 
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.ori.friendsmod.FriendsMod;
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, FriendsMod.MOD_ID);
+
+    public static RegistryObject<Item> DOCUMENT = ITEMS.register("agreement_document",
+            () -> new Item(new Item.Properties()));
+
+    public static void register (IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
 }
